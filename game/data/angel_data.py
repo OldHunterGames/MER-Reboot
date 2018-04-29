ab279cc76f84180232180c4898f996858c8c33f7
Old Huntsman <old_huntsman@yahoo.com>
2018-04-30 00:53:02 +0300

    angel data

---
+++ game/data/angel_data.py
@@ -0,0 +1,19 @@
+## Data for angels
+
+seraph_names = {'serpis': __('Michael'), 'kamira': __('Gabriel'), 'pardus': __('Raphael'), 'corvus': __('Metathron')}
+
+cherub_names = [__('Ariel'), __('Boel'), __('Kamael'), __('Uziel'), ]
+
+ellochim_names = [ __('Abalim'), __('Barakhim'), __('Herelim'), __('Geborlim'), __('Irilim'), __('Gebraim'), __('Kerubim'), __('Nephilim'), ]
+
+archon_first_names = [ __('Paladron'), __('Belerophon'), __('Akhtanton'), __('Archiniton'), __('Autakhton'), __('Echnatron'), __('Balatron'), __('Babilon'), __('Haramon'), __('Celeron'), __('Cherubion'), __('Clarion'),
+__('Armatron'), __('Daeron'), __('Dolothron'), __('Volodolbon'), __('Deseptron'), __('Elgarion'), __('Ezephitron'), __('Faratron'), __('Fluxion'), __('Futurion'), __('Garmatron'), __('Gelirion'), __('Googleon'), __('Irpetron'),
+__('Intelipn'), __('Interotron'), __('Jerikhon'), __('Jabberion'), __('Juggertron'), __('Kholiston'), __('Keniatron'), __('Klamidrion'), __('Lazarion'), __('Luretrion'), __('Labiatron'), __('Melachan'), __('Mergatron'), __('Mazarion'),
+__('Needlion'), __('Nodetron'), __('Nazalatron'), __('Ophuion'), __('Opalatron'), __('Onyxion'), __('Peltatron'), __('Parafion'), __('Purgatron'), __('Paragon'), __('Razeron'), __('Rebellon'), __('Rubytron'), __('Sadistron'),
+__('Sealophon'), __('Sarmatron'), __('Strobophon'), __('Phenomenon'), __('Torophon'), __('Pornotron'), __('Teletron'), __('Laringophon'), __('Trumaliphon'), __('Traumaton'), __('Taumaturgyon'), __('Tetragrammatron'), __('Gramarnazion'), __('Urbanion'),
+__('Xephon'), __('Zephtrion'), __('Xenon'), __('Xenotron'), __('Umberyphon'), __('Ultramrion'), __('Altron'), __('Ultron'), __('Ultimatron'), __('Yahoophon'), __('Yellynghophon'), __('Urbatron'), __('Zlatotron'), __('Passlogion'), ]
+
+
+archon_second_names = [ __('Almud'), __('Aarim'), __('Adimus'), __('Achiakh'), __('Anakhita'), __('Abakh'), __('Anakhim'), __('Arakhib'), __('Arioch'), __('Ariukh'), __('Armaros'), __('Asaph'),
+__('Dumah'), __('Gezuriya'), __('Hamon'), __('Haroth'), __('Epoch'), __('Hemah'), __('Irin'), __('Kezef'), __('Malakh'), __('Maroth'), __('Merkabah'), __('Nakir'), __('Purah'), __('Rahab'),
+__('Sabaoth'), __('Zephon'), __('Zephirion'), __('Qaddisin'), __('Rabdos'), __('Talmud'), __('Sephyrot'), __('Talmud'), __('Mochiakh'), ]
